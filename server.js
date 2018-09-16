04d8153df080e528692a2bdc9701db190d420189
WingChhun <capt.wing.chhun@gmail.com>
2018-09-15 19:50:51 -0700

    configure rethinkdb

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
 const io = require("socket.io")();
 
+const r = require("rethinkdb");
+
 const PORT = process.env.PORT || 8000;
+const RETHINK_PORT = process.env.RETHINK || 28015;
 
-//*Connection, passback a callback with that client
-io.on("connection", (client) => {
+//TODO: Sockets will depend on connecting to the database
 
-    /*
+r
+    .connect({host: 'localhost', port: RETHINK_PORT, db: 'awesome_whiteboard'})
+    .then((connection) => {
+
+        //*Connection, passback a callback with that client
+        io.on("connection", (client) => {
+
+            /*
 function : client.on("subscribeToTimer")
 @desc: Will fire once a subscribeToTimer is fired from the client
     */
-   
-    client.on("subscribeToTimer", (interval) => {
 
-        console.log("Client is subscriming to timer with interval", interval);
+            client.on("subscribeToTimer", (interval) => {
 
-        setInterval(() => {
+                console.log("Client is subscriming to timer with interval", interval);
 
-            client.emit("timer", new Date());
+                r
+                    .table('timers')
+                    .changes()
+                    .run(connection)
+                    .then((cursor) => {
+                        cursor.each((err, timerRow) => {
 
-        }, interval)
+                            client.emit("timer", timerRow.new_val.timestamp);
+
+                        });
+                    });
+                //     setInterval(() => {         client.emit("timer", new Date());     },
+                // interval) });
+
+            });
+
+        });
     });
-
-});
-
+    
 io.listen(PORT);
 
 console.log("Listening on port", PORT);
